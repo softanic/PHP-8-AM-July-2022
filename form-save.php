e2f80ff1ce110bef4f97d34d695812d26c1b1461
softanic <info@softanic.in>
2022-09-13 17:12:24 +0530

    1

--- form-save.php
+++ form-save.php
@@ -1,7 +1,8 @@
 <?php
 //echo 'data saved successfully';
-print_r($_REQUEST);
-
+//print_r($_REQUEST);
+echo "<br>";
+echo implode(', ',$_REQUEST['lang']);
 
 ?>
 
--- form.php
+++ form.php
@@ -10,20 +10,22 @@
 			<input type="text" name="mobile"><br>
 			<label>email</label>
 			<input type="email" name="email"><br>
+			<label>Gender</label>
+			<input type="radio" name="gender" value='male' >Male
+			<input type="radio" name="gender" value='female'>Female
+			
+			<br>
+			<label>Lang known</label>
+			<input type='checkbox' name='lang[]' value='marathi'>Marathi
+			<input type='checkbox' name='lang[]' value='eng'>english
+			<input type='checkbox' name='lang[]' value='hindi'>hindi
+			
+			<br>`
+			
+			
 			<input type="submit">
 			
 		</form>
-		<hr>
-		Get method
-		<form method="get" action="form-save.php">
-			<label>name</label>
-			<input type="text" name="name"><br>
-			<label>Mobile no</label>
-			<input type="text" name="mobile"><br>
-			<label>email</label>
-			<input type="email" name="email"><br>
-			<input type="submit">
-			
-		</form>
+		
 	</body>
 </html>
